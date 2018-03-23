44c671ab4770bc677829d677d522256858143b26
Richard Barnes <rbarnes@umn.edu>
2018-03-23 16:45:56 -0700

    Make debugging message more helpful.

--- read_state_shapefile.py
+++ read_state_shapefile.py
@@ -15,15 +15,13 @@ if len(sys.argv)!=3:
 sf = shapefile.Reader(sys.argv[2])
 
 for state in sf.iterShapeRecords():
-  if state.record[2]==sys.argv[1]:
+  if state.record[1]==sys.argv[1]:
     break
 else:
-  raise Exception("Could not find state!")
+  raise Exception("Could not find state '{0}'!".format(sys.argv[1]))
 
 for i in range(1,len(state.shape.points)):
     if i in state.shape.parts:
         print()
     pt = state.shape.points[i]
     print(pt[0], pt[1])
-
-    
