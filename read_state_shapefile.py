628644f7bb642106e2d769b76d30ffdc1cfcc939
Richard Barnes <rbarnes@umn.edu>
2018-03-22 23:41:03 -0700

    Add better error checking and command line argument interface

--- read_state_shapefile.py
+++ read_state_shapefile.py
@@ -7,13 +7,23 @@ and second argument being a filename of a shape record file,
  e.g. "/Users/klein/Downloads/cb_2016_us_state_500k/cb_2016_us_state_500k"
 '''
 
+if len(sys.argv)!=3:
+  print("Syntax: {0} <State Name> <Shapefile of State Outlines>")
+  sys.exit(-1)
+
 #This will print out a sequence of line segments
 sf = shapefile.Reader(sys.argv[2])
-x = next(x for x in sf.iterShapeRecords()  if x.record[4]==sys.argv[1])
-for i in range(1,len(x.shape.points)):
-    if i in x.shape.parts:
+
+for state in sf.iterShapeRecords():
+  if state.record[2]==sys.argv[1]:
+    break
+else:
+  raise Exception("Could not find state!")
+
+for i in range(1,len(state.shape.points)):
+    if i in state.shape.parts:
         print()
-    pt = x.shape.points[i]
+    pt = state.shape.points[i]
     print(pt[0], pt[1])
 
     
