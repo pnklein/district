0c6f02010b36e06af53f0c62c7232bd372d306d0
Richard Barnes <rbarnes@umn.edu>
2018-03-26 09:56:59 -0700

    Generate multiple district plans

--- RUN.sh
+++ RUN.sh
@@ -15,12 +15,15 @@ temp_out_gnuplot=/z/temp_out_gnuplot
 temp_out_districts=/z/temp_out_districts
 
 echo "Reading census blocks..."
-#python3 read_census_blocks.py $pop_file $temp_out_pop
+python3 read_census_blocks.py $pop_file $temp_out_pop
 echo "Reading state boundaries"
-#python3 read_state_shapefile.py $state_name $state_shapefile > $temp_out_state
+python3 read_state_shapefile.py $state_name $state_shapefile > $temp_out_state
 echo "Generating power diagrams"
-#./do_redistrict $district_num $temp_out_pop > $temp_out_power
-#python3 Voronoi_boundaries.py $temp_out_power $temp_out_voronoi
-python3 extract_district_boundaries.py $temp_out_voronoi $temp_out_state > $temp_out_districts
-#python3 plotGNUPlot.py $temp_out_voronoi $temp_out_state $temp_out_gnuplot False
-#gnuplot < $temp_out_gnuplot
+for (( i=0 ; i < 10 ; i++ )) ; do 
+  temp_out_gnuplot="/z/temp_out_gnuplot_${i}"
+  ./do_redistrict $district_num $temp_out_pop > $temp_out_power
+  python3 Voronoi_boundaries.py $temp_out_power $temp_out_voronoi
+  #python3 extract_district_boundaries.py $temp_out_voronoi $temp_out_state > "${temp_out_districts}_${i}"
+  python3 plotGNUPlot.py $temp_out_voronoi $temp_out_state $temp_out_gnuplot False
+  gnuplot < $temp_out_gnuplot
+done
