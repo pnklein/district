28efcb0f3de0edef10274567f32163e8dd9702d8
Richard Barnes <rbarnes@umn.edu>
2018-04-02 12:26:03 -0600

    Handle possibility of multithreading

--- RUN.sh
+++ RUN.sh
@@ -7,19 +7,20 @@ state_name=$2
 state_shapefile=$3
 district_num=$4
 
-temp_out_pop=/z/temp_out_pop
-temp_out_power=/z/temp_out_power
-temp_out_state=/z/temp_out_state
-temp_out_voronoi=/z/temp_out_voronoi
-temp_out_gnuplot=/z/temp_out_gnuplot
-temp_out_districts=/z/temp_out_districts
+rand_str=`tr -dc A-Za-z0-9_ < /dev/urandom | head -c 20`
+temp_out_pop="tmp.census_data.${rand_str}"
+temp_out_power="tmp.power_diag.${rand_str}"
+temp_out_state="tmp.state_boundary.${rand_str}"
+temp_out_voronoi="tmp.voronoi_boundary.${rand_str}"
+temp_out_gnuplot="tmp.gnuplot.${rand_str}"
+temp_out_districts="out_districts.${rand_str}"
 
 echo "Reading census blocks..."
 python3 read_census_blocks.py $pop_file $temp_out_pop
 echo "Reading state boundaries"
 python3 read_state_shapefile.py $state_name $state_shapefile > $temp_out_state
 echo "Generating power diagrams"
-for (( i=0 ; i < 10 ; i++ )) ; do 
+for (( i=0 ; i < 1 ; i++ )) ; do 
   temp_out_gnuplot="/z/temp_out_gnuplot_${i}"
   ./do_redistrict $district_num $temp_out_pop > $temp_out_power
   python3 Voronoi_boundaries.py $temp_out_power $temp_out_voronoi
@@ -27,3 +28,5 @@ for (( i=0 ; i < 10 ; i++ )) ; do
   #python3 plotGNUPlot.py $temp_out_voronoi $temp_out_state $temp_out_gnuplot False
   #gnuplot < $temp_out_gnuplot
 done
+
+rm -f $temp_out_pop $temp_out_power $temp_out_state $temp_out_voronoi $temp_out_gnuplot
