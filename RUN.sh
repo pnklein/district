384bca5716e4c49d4106fd873d18bc8f137cd2a7
Richard Barnes <rbarnes@umn.edu>
2018-04-02 14:12:04 -0600

    Fix script for running within other scripts

--- RUN.sh
+++ RUN.sh
@@ -1,32 +1,38 @@
 #!/bin/bash
 
-echo "Syntax: $0 <Population File> <State Name> <States Outline> <District Num>"
+if [ "$#" -ne 5 ]; then
+  echo "Syntax: $0 <Population File> <State Name> <States Outline> <District Num> <Output Name>"
+  exit 1
+fi
 
 pop_file=$1
 state_name=$2
 state_shapefile=$3
 district_num=$4
+output_name=$5
 
-rand_str=`tr -dc A-Za-z0-9_ < /dev/urandom | head -c 20`
-temp_out_pop="tmp.census_data.${rand_str}"
-temp_out_power="tmp.power_diag.${rand_str}"
-temp_out_state="tmp.state_boundary.${rand_str}"
-temp_out_voronoi="tmp.voronoi_boundary.${rand_str}"
-temp_out_gnuplot="tmp.gnuplot.${rand_str}"
-temp_out_districts="out_districts.${rand_str}"
+rand_str=`tr -dc A-Za-z0-9_ < /dev/urandom | head -c 20` #Prevents issues when using parallelism
+temp_out_pop="temp_census_data_${state_name}_${rand_str}"
+temp_out_power="temp_power_diag_${state_name}_${rand_str}"
+temp_out_state="temp_state_boundary_${state_name}_${rand_str}"
+temp_out_voronoi="temp_voronoi_boundary_${state_name}_${rand_str}"
+temp_out_gnuplot="temp_gnuplot_${state_name}_${rand_str}"
 
 echo "Reading census blocks..."
 python3 read_census_blocks.py $pop_file $temp_out_pop
-echo "Reading state boundaries"
+
+echo "Reading state boundaries..."
 python3 read_state_shapefile.py $state_name $state_shapefile > $temp_out_state
-echo "Generating power diagrams"
-for (( i=0 ; i < 1 ; i++ )) ; do 
-  temp_out_gnuplot="/z/temp_out_gnuplot_${i}"
-  ./do_redistrict $district_num $temp_out_pop > $temp_out_power
-  python3 Voronoi_boundaries.py $temp_out_power $temp_out_voronoi
-  python3 extract_district_boundaries.py $temp_out_voronoi $temp_out_state > "${temp_out_districts}_${i}"
-  #python3 plotGNUPlot.py $temp_out_voronoi $temp_out_state $temp_out_gnuplot False
-  #gnuplot < $temp_out_gnuplot
-done
+
+echo "Generating power diagram..."
+./do_redistrict $district_num $temp_out_pop > $temp_out_power
+
+echo "Generating Voronoi boundaries..."
+python3 Voronoi_boundaries.py $temp_out_power $temp_out_voronoi
+
+echo "Extracting districting boundaries..."
+python3 extract_district_boundaries.py $temp_out_voronoi $temp_out_state > "$output_name"
+#python3 plotGNUPlot.py $temp_out_voronoi $temp_out_state $temp_out_gnuplot False
+#gnuplot < $temp_out_gnuplot
 
 rm -f $temp_out_pop $temp_out_power $temp_out_state $temp_out_voronoi $temp_out_gnuplot
